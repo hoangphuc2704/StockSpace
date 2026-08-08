96ce38d2dbf37b629eb84fb10f222ce0ebcf5ba7
CUONGBUII <cuongbb14@gmail.com>
2026-08-09 04:04:57 +0700

    Refactor layoutApi and warehouseApi: Comment out tenant and owner layout methods in layoutApi; add them in warehouseApi. Remove src_filelist.txt.

--- src/services/warehouse/warehouseApi.js
+++ src/services/warehouse/warehouseApi.js
@@ -15,9 +15,27 @@ const onwerwarehouseApi = {
     return api.get(`/warehouses/${warehouseId}/layout`)
   },
 
+  // Layout của Owner
+  getOwnerWarehouseLayout: (warehouseId) => {
+    return api.get(`/owner/warehouses/${warehouseId}/layout`)
+  },
+
+  saveOwnerWarehouseLayout: (warehouseId, data) => {
+    return api.put(`/owner/warehouses/${warehouseId}/layout`, data)
+  },
+
+  // Layout riêng của Tenant
+  getTenantWarehouseLayout: (warehouseId) => {
+    return api.get(`/tenant/warehouses/${warehouseId}/layout`)
+  },
+
+  saveTenantWarehouseLayout: (warehouseId, data) => {
+    return api.put(`/tenant/warehouses/${warehouseId}/layout`, data)
+  },
+
   //cập nhật thông tin kho
   updateWarehouseInfo: (warehouseId, data) => {
-    return api.put(`/owner/warehouses/${warehouseId}`, data)
+    return api.put(`/owner/warehouses/${warehouseId}/layout`, data)
   },
 
   //xóa kho
